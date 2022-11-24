af6f1c6003e59789d46a0e7aa77368676870ad0a
Radu Andrei <radu_andrei@trimble.com>
2022-11-24 10:25:03 +0200

    nx workspace and ddd for libs

---
+++ libs/luggage/feature-report-loss/src/lib/luggage-feature-report-loss.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LuggageDomainModule } from '@flight-workspace/luggage/domain';
+import { ReportLossComponent } from './report-loss.component';
+import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
+import { FlightLibModule } from '@flight-workspace/flight-lib';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LuggageDomainModule,
+    //LuggageFeatureCheckinModule,
+    //FlightLibModule,
+  ],
+  declarations: [ReportLossComponent],
+  exports: [ReportLossComponent],
+})
+export class LuggageFeatureReportLossModule {}
