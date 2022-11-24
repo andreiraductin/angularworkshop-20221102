18d6a891f512b3bf7cb8408f92f94e3d6f6ba7f1
Radu Andrei <radu_andrei@trimble.com>
2022-11-24 11:39:24 +0200

    used ngRx to load flights

---
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -0,0 +1,22 @@
+import { Flight } from '@flight-workspace/flight-lib';
+import { createAction, props } from '@ngrx/store';
+
+export const loadFlightBookings = createAction(
+  '[FlightBooking] Load FlightBookings'
+);
+
+export const loadFlightBookingsSuccess = createAction(
+  '[FlightBooking] Load FlightBookings Success',
+  props<{ data: any }>()
+);
+
+export const loadFlightBookingsFailure = createAction(
+  '[FlightBooking] Load FlightBookings Failure',
+  props<{ error: any }>()
+);
+
+export const flightsLoaded = createAction(
+  '[FlightBooking] FlightsLoaded',
+  props<{flights: Flight[]}>()
+);
+
